7808558796437e16b7a4013dfa5b9e9944cc3374
Joseph Hermansen <90590862+hjoeyy@users.noreply.github.com>
2025-09-29 12:09:21 -0700

    nothing happened
    yeah nothing happened

---
+++ myReactApp/src/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <img
+      src="https://images.unsplash.com/photo-1731370963892-32c7347cd2d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
+      alt="Profile image"
+    />
+  );
+}
+
+export default Profile;
